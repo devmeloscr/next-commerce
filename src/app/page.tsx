10c5c2a3e4d3bac9466748604c0386bbfeb4a4d5
MeloSCR <melocraft888@gmail.com>
2024-02-15 10:11:34 -0300

    Adicionando navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Next E-Commerce 13
-    </main>
+    <div>
+      Next E-commerce 13
+    </div>
   );
 }
